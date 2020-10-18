1436666b3cd158b27806e8d6da5463294873c6e3
riest817 <sano817@hotmail.co.jp>
2020-10-18 16:15:44 +0900

    Initialize repository

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Icon from './correct.gif';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return <Counter />
 }
 
+class Counter extends Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      kanji : "日本",
+      value  : '',
+      answer : 'にほん'
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) { this.setState({value: event.target.value});  }
+
+  handleSubmit(event) {
+    //this.setState({value: event.target.value});
+    console.log(this.state.answer);
+    console.log(this.state.value);
+    if ( this.state.answer === this.state.value ) {
+      alert("正解です！！");
+      const IconComponent = () =>{
+        return <img src={Icon}  alt="アイコン" />
+      }
+      //export default IconComponent
+    } else {
+      alert("間違いです。もう一度回答してみてください！");
+    } 
+    event.preventDefault();
+  }
+
+  render()
+  {
+    return (
+      <div class="main">
+        <h1>漢字ドリル</h1>
+        <div class="question">
+          漢字：{this.state.kanji}
+        </div>
+        <div class="answer">
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              読み: 
+              <textarea value={this.state.value} onChange={this.handleChange} />        
+            </label>
+            <input type="submit" value="送信する" class="button"/>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
